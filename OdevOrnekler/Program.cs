3fbca151e0c27467b761fbb0df4dcc034850ea51
Yusuf0665 <95577518+Yusuf0665@users.noreply.github.com>
2022-12-06 00:25:11 +0300

    Add project files.

---
+++ OdevOrnekler/Program.cs
@@ -0,0 +1,40 @@
+﻿//slaytlarda çıktıları olmayan örnekler
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdevOrnekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secim;
+
+            do
+            {
+                Console.WriteLine("İşlemler\n==========");
+                Console.WriteLine("1-Toplama");
+                Console.WriteLine("2-Çıkarma");
+                Console.WriteLine("3-Çarpma");
+                Console.WriteLine("4-Bölme");
+                Console.WriteLine("0-Çıkış\n");
+                Console.Write("İşleminizi seçiniz: ");
+                secim = Convert.ToInt32(Console.ReadLine());
+                switch (secim)
+                {
+                    case 1: Console.WriteLine("Toplama işlemi seçildi."); break;
+                    case 2: Console.WriteLine("Çıkarma işlemi seçildi."); break;
+                    case 3: Console.WriteLine("Çarpma işlemi seçildi."); break;
+                    case 4: Console.WriteLine("Bölme işlemi seçildi."); break;
+                    case 0: Console.WriteLine("Çıkış seçildi."); break;
+                    default: Console.WriteLine("Yanlış seçim."); break;
+                }
+            }
+            while (secim != 0);
+        }
+    }
+}
